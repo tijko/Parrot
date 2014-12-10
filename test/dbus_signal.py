3d242dbfb2a413aa8e4d5d5699d83b57dd732b9d
Tim Konick <konick781@gmail.com>
2014-12-10 17:04:01 -0500

    dbus signal test

--- test/dbus_signal.py
+++ test/dbus_signal.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import dbus
+import dbus.service
 from gi.repository import GObject
 from dbus.mainloop.glib import DBusGMainLoop
 
@@ -10,9 +10,14 @@ def callback(msg):
     print msg
 
 def parrot_proxy():
-    bus = dbus.SessionBus(mainloop=DBusGMainLoop())
-    proxy = bus.get_object('org.Parrot', '/org/Parrot')
-    proxy.connect_to_signal('accessed', callback, dbus_interface='org.Parrot.Inotify')
+    conn = dbus.bus.BusConnection(
+                    'unix:path=/run/user/1000/dbus/user_bus_socket',
+                     mainloop=DBusGMainLoop()
+                                 )
+    bus = dbus.service.BusName('org.Parrot', conn)
+    proxy = conn.get_object(bus.get_name(), '/org/Parrot')
+    proxy.connect_to_signal('accessed', callback, 
+                            dbus_interface='org.Parrot.Inotify')
     loop = GObject.MainLoop()
     loop.run()
 
