508b58776b7ea867dd701203d3c8ca06ac00c0f9
Tim Konick <konick781@gmail.com>
2014-12-17 23:37:39 -0500

    use declarations for add_watch

--- src/parrot.h
+++ src/parrot.h
@@ -7,18 +7,27 @@
 #include <time.h>
 
 
-// Macro to set a directory to monitor for events. Edit to specify which 
-// directory to monitor.
-#define PARROT_PATH "" 
-
-#define PARROT_SIZE strlen(PARROT_PATH) + 1
-
 // Macro to set a directory to backup the files from the directory above being 
 // monitored.
 // Edit to specify which directory to backup to.
-#define BACKUP_PATH ""
+#define BACKUP_PATH "/home/tijko/Documents/backup/"
 
 #define BACKUP_SIZE strlen(BACKUP_PATH) + 1
+
+struct parrot_watch {
+    int parrot_wd;
+    char *dir;
+};
+
+struct watch_trigger {
+    char *file;
+    char *dir;
+};
+
+struct parrot_watch *current_watch[4092];
+
+int parrot_inotify_instance;
+int watch_num;
 
 // Macro to set the size of an event
 #define EVT_SIZE sizeof(struct inotify_event) + NAME_MAX + 1
@@ -46,12 +55,18 @@ gboolean parrot_obj_accessed(ParrotObject *p_obj, int access_time);
 gboolean parrot_obj_current_watch(ParrotObject *p_obj,
                                   DBusGMethodInvocation *ctxt);
 
+gboolean parrot_obj_method_arg(ParrotObject *p_obj, char *fn,
+			       DBusGMethodInvocation *ctxt);
 // Creates the daemon.
 int parrot_daemon(void);
 
 // This is the function that sets the inotify event loop.  The watch is added for 
 // the `file_dir_to_parrot` and will signal to backup on events.
-int notify_parrot(void);
+int notify_parrot_init(void);
+
+void parrot_add_watch(char *path);
+
+void parrot_mainloop(ParrotObject *p_obj);
 
 // Function to parse the inotify_event structs and pass on the relavent 
 // members.
@@ -59,7 +74,7 @@ void parse_events(int e_status, char e_buf[], ParrotObject *p_obj);
 
 // Walks the directory being monitored and calls the backup function when a 
 // file is found.
-void find_files(char *file_accessed);
+void find_files(struct watch_trigger *accessed);
 
 char *create_pathname(char *dirname, char *filename, size_t pathsize);
 
@@ -95,6 +110,8 @@ void log_backup(char *fn);
 
 // Logs the a successful dbus session connection
 void log_dbus(char *addr);
+
+void log_method(char *fn);
 
 // Signal handler to complete cleanup if any interrupts are received.
 void cleanup(int signo);
