82c6c8d0fdb3064514c1564bafcf498e418901c1
Tim Konick <konick781@gmail.com>
2015-01-07 13:06:39 -0500

    func. to create a pid file

--- src/daemon.c
+++ src/daemon.c
@@ -1,3 +1,5 @@
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 
@@ -6,13 +8,12 @@
 
 int parrot_daemon(void) 
 {
-
     /*
      * Creates a daemon out the Parrot process.  Parrot will continue to run
-     * threads separated from any controlling terminal.  This is the standard
+     * threads separated from any controlling terminal.  This is the standard 
      * method for creating a daemon in a linux/gnu enviroment.  Fork twice,
-     * setting the file mode, create a new session ID, and finally change to the 
-     * "/" or root directory.
+     * setting the file mode, create a new session ID, and finally change to 
+     * the "/" or root directory are all still done.
      */
    
     pid_t pid;
@@ -33,6 +34,15 @@ int parrot_daemon(void)
     if ((chdir("/")) == -1) 
         return -1;       // change directory to root "/"
 
+
+    pid = fork();  // second fork
+    if (pid == -1)
+        return -1;
+
+    if (pid > 0)
+        exit(0);
+
+    create_pid_file();
     close(STDIN_FILENO); // close stdin, stdout, stderr
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
@@ -40,3 +50,38 @@ int parrot_daemon(void)
     return 0;
 }
 
+int create_pid_file(void)
+{
+    char *runtime_dir, *user_path;
+    int pid_file_fd;
+    pid_t parrot_pid;
+    size_t max_pidstr_len, pathlen;
+
+    parrot_pid = getpid();
+    
+    max_pidstr_len = 10;
+    char pid_str[max_pidstr_len];
+
+    runtime_dir = getenv(XDG_RUNTIME_DIR);
+    if (runtime_dir == NULL)
+        return -1;
+
+    pathlen = strlen(runtime_dir) + strlen(PARROT_PID_PATH) + 1;
+    user_path = malloc(sizeof(char) * pathlen);
+    if (user_path == NULL)
+        return -1;
+
+    snprintf(user_path, pathlen, "%s%s", runtime_dir, PARROT_PID_PATH);
+
+    pid_file_fd = open(user_path, O_CREAT | O_RDWR, 0666);
+    if (pid_file_fd == -1)
+        return -1;
+
+
+    snprintf(pid_str, max_pidstr_len, "%d", parrot_pid);
+    write(pid_file_fd, (void *) pid_str, strlen(pid_str));
+    close(pid_file_fd);
+
+    free(user_path);
+    return 0;
+}
