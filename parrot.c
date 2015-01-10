1f6f8253df06586a30b694446f7d2194d40ca1ec
Tim Konick <konick781@gmail.com>
2015-01-09 22:09:01 -0500

    using new error logging function

--- parrot.c
+++ parrot.c
@@ -6,19 +6,23 @@
 int main(int argc, char *argv[]) {
 
     int notify_flag;
-    if ((signal(SIGINT, cleanup)) == SIG_ERR)
-        sig_err();
-
+    if ((signal(SIGINT, cleanup)) == SIG_ERR) {
+        log_error("main.c", "main", "signal", 9);
+        return 0;
+    }
+    
     notify_flag = parrot_daemon();
 
-    if (notify_flag == -1)
-        notify_err();
+    if (notify_flag == -1) {
+        log_error("main.c", "main", "parrot_daemon", 12);
+        return 0;
+    }
 
     RUNNING = true;
     notify_flag = notify_parrot_init();
 
     if (notify_flag == -1)
-        notify_err();
+        log_error("main.c", "main", "notify_parrot_init", 18);
 
     return 0;
 }
