e59213ecb8a6e7fddfb032a7197d06af9d78e843
Tim Konick <konick781@gmail.com>
2015-07-06 10:51:56 -0400

    updated test

--- test/parrot_test.py
+++ test/parrot_test.py
@@ -28,15 +28,14 @@ def parrot_proxy():
     proxy.connect_to_signal('accessed', callback, 
                             dbus_interface='org.Parrot.Inotify')
 
-    watch_method = proxy.get_dbus_method('current_watch')
+    watch_method = proxy.get_dbus_method('current_watches')
     add_watch = proxy.get_dbus_method('add_watch')
     print "Calling 'add_watch' method..."
-    add_watch(user_test_path)
+    add_watch(user_test_path, 1)
     print "Calling 'current_watch' method..."
     cur_watch = watch_method() 
     print "Currently watched: %s" % cur_watch
     print "Waiting on signal 'accessed'..."
-    
     loop = GObject.MainLoop()
     loop.run()
 
