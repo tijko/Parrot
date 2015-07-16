275ef3fbc068bc445b55bcf044b40d2bdc3dd625
Tim Konick <konick781@gmail.com>
2015-07-15 22:15:39 -0400

    set evfile

--- src/notify_parrot.c
+++ src/notify_parrot.c
@@ -82,14 +82,20 @@ void parrot_mainloop(struct ParrotGDBusObj *parrot_gdbus_obj)
     parrot_cleanup(parrot_gdbus_obj);
 }
 
-int parrot_add_watch(char *path, int mask)
+int parrot_add_watch(char *watch_path, char *backup_path, int mask)
 {
     int watch, f_open, d_open;
     struct parrot_watch *new_watch;
 
+    d_open = open(backup_path, O_RDONLY | O_DIRECTORY);
+    if (d_open == -1)
+        return -1;
+    else
+        close(d_open);
+ 
     if (mask & W_FIL) {
-        f_open = open(path, O_RDONLY);
-        d_open = open(path, O_RDONLY | O_DIRECTORY);    
+        f_open = open(watch_path, O_RDONLY);
+        d_open = open(watch_path, O_RDONLY | O_DIRECTORY);    
         if (f_open == -1 || d_open != -1) {
             close(f_open);
             close(d_open);
@@ -97,7 +103,7 @@ int parrot_add_watch(char *path, int mask)
             return -1;
         }
     } else {
-        d_open = open(path, O_RDONLY | O_DIRECTORY);
+        d_open = open(watch_path, O_RDONLY | O_DIRECTORY);
         if (d_open == -1) {
             log_error("notify_parrot.c", "parrot_add_watch", "open", 100);
             return -1;
@@ -112,33 +118,40 @@ int parrot_add_watch(char *path, int mask)
     }
 
     if ((watch = inotify_add_watch(parrot_inotify_instance, 
-                                   path, IN_ACCESS)) == -1) {
+                                   watch_path, IN_ACCESS)) == -1) {
         free(new_watch);
         log_error("notify_parrot.c", "parrot_add_watch", 
                   "inotify_add_watch", 114);
         return -1;
     }
 
-    new_watch->path = malloc(sizeof(char) * (strlen(path) + 1));
-    strcpy(new_watch->path, path);
+    new_watch->watch_path = malloc(sizeof(char) * (strlen(watch_path) + 1));
+    strcpy(new_watch->watch_path, watch_path);
+    new_watch->backup_path_len = strlen(backup_path) + 1;
+    new_watch->backup_path = malloc(sizeof(char) * new_watch->backup_path_len);
+    strcpy(new_watch->backup_path, backup_path);
     new_watch->parrot_wd = watch;
     new_watch->watch_type = mask;
     new_watch->backup = mask & W_FIL ? find_file : find_files;
+    if (new_watch->watch_type & W_FIL) 
+        set_evfile(new_watch);
+    else
+        new_watch->evfile = NULL;
     current_watch[watch_num++] = new_watch;
-    log_event("watch added => ", 1, path);
+    log_event("watch added => ", 1, watch_path);
     return 0;
 }
 
-void parrot_remove_watch(char *path)
+void parrot_remove_watch(char *watch_path)
 {
     int watch, idx;
 
     for (watch=0; watch < watch_num; watch++) {
-        if (!strcmp(path, current_watch[watch]->path)) {
-            free(current_watch[watch]->path);
+        if (!strcmp(watch_path, current_watch[watch]->watch_path)) {
+            free(current_watch[watch]->watch_path);
             free(current_watch[watch]);
             current_watch[watch] = NULL;
-            log_event("watch removed => ", 1, path);
+            log_event("watch removed => ", 1, watch_path);
             for (idx=watch; watch < watch_num; watch++) 
                 if (current_watch[watch])
                     current_watch[idx++] = current_watch[watch];
@@ -146,6 +159,26 @@ void parrot_remove_watch(char *path)
             return;
         }
     }
+}
+
+void set_evfile(struct parrot_watch *watch)
+{
+    char *path, *prev_path, *temp_path, *watch_path;
+
+    watch_path = malloc(sizeof(char) * (strlen(watch->watch_path) + 1));
+    strcpy(watch_path, watch->watch_path);
+    prev_path = NULL;
+    strtok(watch_path, "/");
+
+    while ((path = strtok(NULL, "/"))) {
+        temp_path = realloc(prev_path, sizeof(char) * (strlen(path) + 1));
+        strcpy(temp_path, path);
+        prev_path = temp_path;
+    } 
+
+    watch->evfile = malloc(sizeof(char) * (strlen(prev_path) + 1));
+    strcpy(watch->evfile, prev_path);
+    free(prev_path);
 }
 
 int events_in(int highest_fd, fd_set *watchfds)
@@ -187,7 +220,13 @@ void parse_events(int e_status, char e_buf[], ParrotObject *p_obj)
         for (cur_watch=0; cur_watch < watch_num; cur_watch++) {
             if (current_watch[cur_watch]->parrot_wd == event->wd) {
                 memcpy(accessed, current_watch[cur_watch], sizeof(*accessed));
-                accessed->evfile = event->name;
+                if (!(accessed->watch_type & W_FIL)) {
+                    if (accessed->evfile)
+                        free(accessed->evfile);
+                    accessed->evfile = malloc(sizeof(char) * 
+                                              strlen(event->name) + 1);
+                    strcpy(accessed->evfile, event->name);
+                }
                 break;
             }
         }
