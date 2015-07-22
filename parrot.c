3c8ae002d697db5e173ee6444e7c131ecd6dd2e2
Tim Konick <konick781@gmail.com>
2015-07-22 11:59:54 -0400

    reserve caps for macors

--- parrot.c
+++ parrot.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    RUNNING = true;
+    running = true;
     notify_flag = notify_parrot_init();
 
     if (notify_flag == -1)
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
 
 void cleanup(int signo)
 {
-    RUNNING = false;
+    running = false;
     sleep(1); // a small sleep to allow enough time for cleanup.
     log_event("signal interrupt", 0);
     signal(SIGINT, SIG_DFL);
